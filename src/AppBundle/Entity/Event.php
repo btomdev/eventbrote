eb1bf0a68db0ce4bafbfaa7ab3ac4afe27f17d5e
Tom-Ben-Azerat <tom.benazerat@gmail.com>
2017-11-26 00:18:36 +0100

    Connexion à la base de donnée eventbrot, création de l'Entity Event et de la table event

---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="events")
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=2)
+     */
+    private $price;
+
+
+
+}
